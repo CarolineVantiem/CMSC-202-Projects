0d3cdd4a0a08b1e93a63df0f8295c7bcc092a089
cvantie1 <cvantie1@linux6.gl.umbc.edu>
2019-10-25 12:41:39 -0400

    proj2

---
+++ proj2/proj2.cpp
@@ -0,0 +1,17 @@
+//Title:   Project 2 for Spring 2018
+// Author: Caroline Vantiem
+
+#include "Dragon.h"
+#include "Game.h"
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main () {
+  Game newGame;
+  newGame.StartGame();
+  return 0;
+}
